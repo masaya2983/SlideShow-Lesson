ed2b56296b7c12ffde68d8475390fc2700d8fb60
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-05-30 06:44:01 +0000

    ver up

---
+++ script.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper',{
+  loop: true,
+
+  pageination: {
+    el: '.swiper-pagination',
+  },
+  navigation: {
+    nextEl:'.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+})
